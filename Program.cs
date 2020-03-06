1b21c9acbba2681918f6e613bcb3d1936652be92
Stephen Kalpin <skalpin@gmail.com>
2020-03-06 09:44:46 -0600

    adding dotnet core console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace advanced_git_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
